0f128628982d301a654318f130b91f86c8e51ac0
Harry <harshchoudhary1343@gmail.com>
2026-06-02 21:41:02 +0530

    Day 6: Add QuizAttempt and UserAnswer models

--- backend/quizzes/admin.py
+++ backend/quizzes/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Subject, Topic, Question, Option
+from .models import (
+    Subject,
+    Topic,
+    Question,
+    Option,
+    QuizAttempt,
+    UserAnswer
+)
 
 admin.site.register(Subject)
 admin.site.register(Topic)
 admin.site.register(Question)
 admin.site.register(Option)
+admin.site.register(QuizAttempt)
+admin.site.register(UserAnswer)
--- backend/quizzes/models.py
+++ backend/quizzes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Subject(models.Model):
@@ -56,3 +57,53 @@ class Option(models.Model):
 
     def __str__(self):
         return self.option_text
+
+
+class QuizAttempt(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='quiz_attempts'
+    )
+
+    subject = models.ForeignKey(
+        Subject,
+        on_delete=models.CASCADE
+    )
+
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE
+    )
+
+    score = models.IntegerField(default=0)
+    total_questions = models.IntegerField(default=0)
+
+    started_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.topic.name}"
+
+
+class UserAnswer(models.Model):
+    quiz_attempt = models.ForeignKey(
+        QuizAttempt,
+        on_delete=models.CASCADE,
+        related_name='answers'
+    )
+
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+
+    selected_option = models.ForeignKey(
+        Option,
+        on_delete=models.CASCADE
+    )
+
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.quiz_attempt.user.username} - Q{self.question.id}"
